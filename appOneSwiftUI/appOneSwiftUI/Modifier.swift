57a9bbd454c0452f61054b5ebebbef7502af8d09
MattJeff <mhiguinen235@gmail.com>
2022-09-08 17:06:28 +0200

    Font Custom

---
+++ appOneSwiftUI/appOneSwiftUI/Modifier.swift
@@ -0,0 +1,34 @@
+//
+//  Modifier.swift
+//  appOneSwiftUI
+//
+//  Created by Mathis Higuinen on 08/09/2022.
+//
+
+import SwiftUI
+
+
+
+struct shadowModifier:ViewModifier{
+    func body(content: Content) -> some View {
+        content
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+            .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 10)
+    }
+}
+struct FontModifier:ViewModifier{
+    var style: Font.TextStyle = .body
+    func body(content: Content) -> some View {
+        content
+            .font(.system(style,design: .default))
+    }
+}
+
+
+struct CustomFontModifier:ViewModifier{
+    var size:CGFloat = 28
+    var name = "MPLUSRounded1c-Bold"
+    func body(content: Content) -> some View {
+        content.font(.custom(name, size: size))
+    }
+}
